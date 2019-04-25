3846a7e9dc53e43e3220e19a5e1c116563ed150a
wenhuangzeng <36428519+wenhuangzeng@users.noreply.github.com>
2019-04-24 22:50:45 -0400

    Login and Signup works without any hashing

--- src/main/java/com/genfit/userfacing/handlers/SignupHandler.java
+++ src/main/java/com/genfit/userfacing/handlers/SignupHandler.java
@@ -1,4 +1,54 @@
 package com.genfit.userfacing.handlers;
 
-public class SignupHandler {
+import com.genfit.database.Database;
+import com.google.common.collect.ImmutableMap;
+import com.google.gson.Gson;
+import jdk.nashorn.internal.ir.annotations.Immutable;
+import org.mindrot.jbcrypt.BCrypt;
+import spark.QueryParamsMap;
+import spark.Request;
+import spark.Response;
+import spark.Route;
+
+import java.sql.SQLException;
+import java.util.Map;
+
+public class SignupHandler implements Route {
+
+  private static final Gson GSON = new Gson();
+  private Database db;
+  public SignupHandler(Database db) {
+    this.db = db;
+  }
+
+  @Override
+  public String handle(Request req, Response res) {
+    QueryParamsMap qm = req.queryMap();
+    String name = qm.value("name");
+    String username = qm.value("username");
+    String hashPwd = qm.value("password");
+
+    boolean newUsername = true;
+//    String finalHash = BCrypt.hashpw(hashPwd, BCrypt.gensalt(10));
+    String finalHash = hashPwd;
+    // Check database
+    try {
+      newUsername = this.db.checkSignup(username);
+    } catch (Exception e) {
+//      System.out.println("ERROR: Error when checking signup info.");
+      e.printStackTrace();
+    }
+
+    if (newUsername) {
+      // add to database
+      try {
+        this.db.addUser(name, username, finalHash);
+      } catch (SQLException e) {
+        System.out.println("ERROR: error when adding new user to database.");
+      }
+    }
+
+    Map<String, Object> variables = ImmutableMap.of("success", newUsername);
+    return GSON.toJson(variables);
+  }
 }
