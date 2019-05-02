bcbe9b0f50912746f2f18284b5aef9d23d580d6d
Lawr <lawrence_huang1@brown.edu>
2019-05-01 21:35:56 -0400

    fixed item similarity test after algorithm refactor

--- src/test/java/edu/brown/cs/itemsimilarity/ItemSimilarityTest.java
+++ src/test/java/edu/brown/cs/itemsimilarity/ItemSimilarityTest.java
@@ -15,37 +15,37 @@ public class ItemSimilarityTest {
       db = new Database(AWSConnection.getDBConnectionUsingIam());
 
       System.out.println("boat shoes and converses");
-      System.out.println(ItemDistanceCalculator.getSimilarity(
+      System.out.println(ItemDistanceCalculator.areSimilar(
               new ItemProxy(db, 55),
               new ItemProxy(db, 56)));
 
       System.out.println("boat shoes and sandals");
-      System.out.println(ItemDistanceCalculator.getSimilarity(
+      System.out.println(ItemDistanceCalculator.areSimilar(
               new ItemProxy(db, 55),
               new ItemProxy(db, 57)));
 
       System.out.println("converses and sandals");
-      System.out.println(ItemDistanceCalculator.getSimilarity(
+      System.out.println(ItemDistanceCalculator.areSimilar(
               new ItemProxy(db, 56),
               new ItemProxy(db, 57)));
 
       System.out.println("two different basketball shoes (red and black)");
-      System.out.println(ItemDistanceCalculator.getSimilarity(
+      System.out.println(ItemDistanceCalculator.areSimilar(
               new ItemProxy(db, 122),
               new ItemProxy(db, 101)));
 
       System.out.println("two different basketball shoes (red and black)");
-      System.out.println(ItemDistanceCalculator.getSimilarity(
+      System.out.println(ItemDistanceCalculator.areSimilar(
               new ItemProxy(db, 122),
               new ItemProxy(db, 101)));
 
       System.out.println("converse and red tshirt");
-      System.out.println(ItemDistanceCalculator.getSimilarity(
+      System.out.println(ItemDistanceCalculator.areSimilar(
               new ItemProxy(db, 56),
               new ItemProxy(db, 58)));
 
       System.out.println("red shoes and red tshirt");
-      System.out.println(ItemDistanceCalculator.getSimilarity(
+      System.out.println(ItemDistanceCalculator.areSimilar(
               new ItemProxy(db, 122),
               new ItemProxy(db, 58)));
     } catch (Exception e) {
