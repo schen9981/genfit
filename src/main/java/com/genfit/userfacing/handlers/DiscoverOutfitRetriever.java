dcc40b45bb9ff9b09d755eaebefd64aa5cad2a1b
Lawr <lawrence_huang1@brown.edu>
2019-05-01 20:53:13 -0400

    add outfit id and name to JSON

--- src/main/java/com/genfit/userfacing/handlers/DiscoverOutfitRetriever.java
+++ src/main/java/com/genfit/userfacing/handlers/DiscoverOutfitRetriever.java
@@ -2,6 +2,7 @@ package com.genfit.userfacing.handlers;
 
 import com.genfit.attribute.attributevals.TypeEnum;
 import com.genfit.proxy.ItemProxy;
+import com.genfit.proxy.OutfitProxy;
 import com.genfit.suggester.OutfitSuggester;
 import com.genfit.suggester.OutfitSuggestion;
 import com.genfit.userfacing.GenFitApp;
@@ -51,13 +52,19 @@ public class DiscoverOutfitRetriever implements Route {
     List<JsonObject> completeOutfits = new LinkedList<>();
     List<JsonObject> incompleteOutfits = new LinkedList<>();
     for (OutfitSuggestion suggestion : completeOutfitsSuggestion) {
+      OutfitProxy communityOutfit = suggestion.getCommunityOutfit();
       Map<TypeEnum, ItemProxy> communityOutfitComp =
-              suggestion.getCommunityOutfit().getItems();
+              communityOutfit.getItems();
       Map<TypeEnum, ItemProxy> userItems =
               suggestion.getSuggestedItemsForUser();
 
       JsonObject communityOutfitJson =
               this.convertItemsToJsonObj(communityOutfitComp);
+
+      communityOutfitJson.addProperty("id", communityOutfit.getId());
+      communityOutfitJson.addProperty("name",
+              communityOutfit.getOutfit().getName());
+
       JsonObject userItemsJson =
               this.convertItemsToJsonObj(userItems);
 
